82d7fda250da00ad5680bc834cb671df1c8028fd
Imri Tull <111070159+xretraces@users.noreply.github.com>
2023-04-11 12:11:21 -0400

    Create Company.java

---
+++ Company.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+public class Company<T extends Employee> {
+    private String name;
+    private ArrayList<T> eList = new ArrayList<>();
+
+    public Company(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean addEmployee(T employee) {
+        if (!employee.getCompanyName().equals(name)) {
+            System.out.println(employee.getName() + " is not an employee of " + this.name);
+            return false;
+        } else if (eList.contains(employee)) {
+            System.out.println(employee.getName() + " is already confirmed as an employee of " + this.name);
+            return false;
+        } else {
+            eList.add(employee);
+            return true;
+        }
+    }
+
+    public String toString() {
+        String res = "";
+        for (T e : eList) {
+            res += e.toString() + "\n";
+        }
+        return res;
+    }
+}
