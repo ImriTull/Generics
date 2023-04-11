5523b33c636407aabb54b1f7b43b0ddce91f2741
Imri Tull <111070159+xretraces@users.noreply.github.com>
2023-04-11 12:12:26 -0400

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,17 @@
+public class Employee {
+    private String name;
+    private String companyName;
+
+    public Employee(String name, String companyName) {
+        this.name = name;
+        this.companyName = companyName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+}
